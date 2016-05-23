7e347ab876edbcc44cd56ab596c610826a4e0b66
Ivan <ivsucic@fesb.hr>
2016-05-23 18:51:29 +0200

    Base layout

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,10 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+//        return $this->render('default/index.html.twig', array(
+//            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+//        ));
+        
+        return $this->render('base.html.twig');
     }
 }

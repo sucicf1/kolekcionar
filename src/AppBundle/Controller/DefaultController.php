68445d4fb896267bafa1d7872865c5670754f20a
Ivan <ivsucic@fesb.hr>
2016-06-01 22:28:39 +0200

    All templates

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,11 +13,6 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-//        return $this->render('default/index.html.twig', array(
-//            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-//        ));
-        
-        return $this->render('layout.html.twig');
+        return $this->redirectToRoute('item_index');
     }
 }

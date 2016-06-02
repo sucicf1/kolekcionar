74739b34d4bdec16304608289917abad443cc39a
Ivan <ivsucic@fesb.hr>
2016-06-02 19:48:37 +0200

    Corrected errors with sample data

--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -3,27 +3,42 @@
 namespace AppBundle\DataFixtures\ORM;
 
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 
 
-class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface,  ContainerAwareInterface
 {
+    private $container;
+    
+    public function setContainer(ContainerInterface $container=null) 
+    {
+        $this->container=$container;
+    }
+
     public function load(ObjectManager $manager) 
     {
-        $user1=new \AppBundle\Entity\User();
+        $userManager=$this->container->get('fos_user.user_manager');
+        
+        $user1=$userManager->createUser();
         $user1->setEmail('korisnik1@korisnik1.com');
         $user1->setUsername('korisnik1');
-        $user1->setPassword('korisnik1');
+        $user1->setPlainPassword('korisnik1');
         $user1->setName('korisnik1');
         $user1->setSurname('nesto');
+        $user1->setEnabled(true);
+        $user1->setRoles(array('ROLE_USER'));
         
-        $user2=new \AppBundle\Entity\User();
+        $user2=$userManager->createUser();
         $user2->setEmail('korisnik2@korisnik2.com');
         $user2->setUsername('korisnik2');
-        $user2->setPassword('korisnik2');
+        $user2->setPlainPassword('korisnik2');
         $user2->setName('korisnik2');
         $user2->setSurname('prezime');
+        $user2->setEnabled(true);
+        $user2->setRoles(array('ROLE_USER'));
         
         $manager->persist($user1);
         $manager->persist($user2);
